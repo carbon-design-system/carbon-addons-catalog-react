7aea5ae1d5692112e34bf85b73e4a01cfb894c15
Chris Dhanaraj <adhanaraj@gmail.com>
2017-08-21 09:09:50 -0500

    fix(build): file path resolutions, naming conventions (#4)
    * fix(build): update naming conventions, fix file path resolution
    
    * chore(build): bump build number

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export DetailPageSidebar from './components/DetailPageSidebar';
-export DetailPageHighlights from './components/DetailPageHighlights';
+export DetailPageSidebar from './components/DetailPageSidebar/DetailPageSidebar.js';
+export DetailPageHighlights from './components/DetailPageHighlights/DetailPageHighlights.js';
