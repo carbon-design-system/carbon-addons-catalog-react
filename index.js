ba1772cac7efbbae3cb5dfc89c8ec8f9663e8d8b
Aditi Rajagopal <arajagopal@us.ibm.com>
2017-09-27 12:29:15 -0500

    fix(indexjs): fixing file export typo

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export DetailPageSidebar from './components/DetailPageSidebar/DetailPageSidebar.js';
 export DetailPageHighlights from './components/DetailPageHighlights/DetailPageHighlights.js';
+export DetailPageHighlightsSimple from './components/DetailPageHighlightsSimple/DetailPageHighlightsSimple.js';
